96916012d3c3ceba0285e042e1eda60282bd163b
MyPy Tutor <deploy@mypy-tutor.com>
2026-06-06 06:39:30 -0700

    Fix: MutableHeaders has no pop() - use del instead

--- app/security.py
+++ app/security.py
@@ -120,8 +120,10 @@ class SecurityMiddleware(BaseHTTPMiddleware):
             "worker-src 'self';"
         )
         # Remove server fingerprint
-        response.headers.pop("server", None)
-        response.headers.pop("x-powered-by", None)
+        if "server" in response.headers:
+            del response.headers["server"]
+        if "x-powered-by" in response.headers:
+            del response.headers["x-powered-by"]
 
         return response
 
